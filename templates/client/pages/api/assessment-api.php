334c7fec9194378d988f73fe426ae3b8584397a6
Zipho Danca <zipho.danca@m2north.com>
2024-08-26 12:28:02 +0200

    saving image base64 to database

--- templates/client/pages/api/assessment-api.php
+++ templates/client/pages/api/assessment-api.php
@@ -8,6 +8,14 @@ $data = $_POST['data'];
 
 if (!empty($data)) {
     $data = json_decode($data, true);
+
+    global $now, $root, $db;
+
+    $root['blog_posts']['usr_blog_posts_image'] = $data['fields']['file'];
+
+    $item = $root['blog_posts'];
+    $item['table'] = 'usr_blog_posts';
+    $result = $db->query_create($item);
 }
 
 //---the frontend will expect an object in this format: { status: 'success', text: 'message received', data: array() or object }
