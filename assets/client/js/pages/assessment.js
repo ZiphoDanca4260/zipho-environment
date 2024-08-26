c73427fcb8b18e202aac1031df9122ec79b6cfc8
Zipho Danca <zipho.danca@m2north.com>
2024-08-26 12:26:59 +0200

    uploading images and converting to base64

--- assets/client/js/pages/assessment.js
+++ assets/client/js/pages/assessment.js
@@ -1,23 +1,41 @@
-console.log('test')
-
 $(function () {
-    $('[data-fr-action="test-ajax"]').on('click', function () {
+    const convertBase64 = (file) => {
+        return new Promise((resolve, reject) => {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(file);
+
+            fileReader.onload = () => {
+                resolve(fileReader.result);
+            };
+
+            fileReader.onerror = (error) => {
+                reject(error);
+            };
+        });
+    };
+
+    $('[data-fr-action="submit"]').on('click', function () {
         printMessage({ status: 'success', text: 'sending to api' });
 
-        let json = {
-            task: "assessment",
-            endPoint: "assessment-api",
-            initiatorElement: $(this),
-            payload: {
-                fields: { field1: "value1", field2: "value2" }
-            },
-            customFunctions: {
-                success: (res) => {
-                    printMessage({ status: 'info', text: 'response received' });
-                }
-            }
-        }
+        let file = document.querySelector('input[type=file]').files[0];
 
-        ajaxPost(json);
+        convertBase64(file)
+            .then((result) => {
+                let json = {
+                    task: "assessment",
+                    endPoint: "assessment-api",
+                    initiatorElement: $(this),
+                    payload: {
+                        fields: { file: result }
+                    },
+                    customFunctions: {
+                        success: (res) => {
+                            printMessage({ status: 'info', text: 'response received' });
+                        }
+                    }
+                }
+
+                ajaxPost(json);
+            });
     })
 })
